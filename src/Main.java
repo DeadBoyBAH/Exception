d94a57b404937d51c5adde56ffeb87ab3eaa4829
Alexey <tokrar322@mail.ru>
2020-03-05 15:59:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+/**
+ * Это класс, который запускает программу
+ *
+ * @author Pukin Alexey
+ * @version 05.03.2020
+ */
+
+public class Main {
+    public static void main(String[] args) throws NextIntException {
+
+
+        CombatShip space = new CombatShip("space", 50, 25);
+
+        space.showGerb();
+        System.out.println(space.name + " движется? - " + space.isMoving);
+        space.move(20);
+        System.out.println(space.name + " движется? - " + space.isMoving);
+        System.out.println("Корабль " + space.name + " движется со скоростью " + space.getSpeed());
+        space.attackOn();
+        space.attackOff();
+        space.stop();
+        System.out.println(space.name + " движется? - " + space.isMoving);
+        System.out.println();
+
+        TradeShip venecia = new TradeShip("venecia");
+
+        venecia.shipment(50);
+        venecia.move(70);
+        System.out.println("Корабль " + venecia.name + " движется со скоростью " + venecia.getSpeed());
+        System.out.println("Корабль " + venecia.name + " прибыл в Севастопль");
+        venecia.stop();
+        venecia.unloading();
+        System.out.println();
+
+        PassengerShip titanic = new PassengerShip("titanic");
+
+        titanic.landing(100);
+        titanic.debarkation();
+        titanic.landing(130, 15);
+        titanic.move(30);
+        titanic.showGerb();
+
+        // Исключение на ввод целого числа с существующим исключением
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.print("Введите целое число: ");
+            int num = in.nextInt();
+            System.out.println(num);
+        } catch (Exception e) {
+            e.getMessage();
+            System.out.println("К сожалению, Вы ввели не целое число");
+        } finally {
+            System.out.println("Если Вы всё сделали правильно, у вас выведется число");
+        }
+
+        // Исключение с собственным исключением
+        throw new NextIntException("К сожалению, Вы ввели не целое число");
+        try {
+
+            System.out.print("Введите целое число: ");
+            int num2 = in.nextInt();
+            System.out.println(num2);
+
+        } catch (NextIntException e) {
+            e.getMessage();
+        } finally {
+            System.out.println("Если Вы всё сделали правильно, у вас выведется число");
+        }
+
+    }
+
+}
